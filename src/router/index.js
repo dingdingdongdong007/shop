5eeab489e9604c9a5eacc6563d0763b1b325a289
yp <729970783@qq.com>
2021-08-03 14:57:24 +0800

    完成用户列表功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -2,28 +2,44 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import login from '../components/login.vue'
 import Home from '../components/Home.vue'
+import welcome from '../components/welcome.vue'
+import users from '../components/user/users.vue'
 
 Vue.use(VueRouter)
 
-const routes = [{
+const routes = [
+  {
     path: '/',
     redirect: '/login'
-}, {
+  },
+  {
     path: '/login',
     component: login
-}, {
+  },
+  {
     path: '/Home',
-    component: Home
-}]
+    component: Home,
+    redirect: '/welcome',
+    children: [
+      {
+        path: '/welcome',
+        component: welcome
+      },
+      {
+        path: '/users',
+        component: users
+      }
+    ]
+  }
+]
 const router = new VueRouter({
-    routes
+  routes
 })
 
-
 router.beforeEach((to, from, next) => {
-    if (to.path === '/login') return next();
-    const tokenStr = window.sessionStorage.getItem('token');
-    if (!tokenStr) return next('/login');
-    next();
+  if (to.path === '/login') return next()
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
 })
 export default router
